9c5f487d8aaf6ea9e7852f08fc64826ea4bf125b
Madhumitha <167957749+madhumithaponnusamy@users.noreply.github.com>
2026-03-02 11:04:48 +0530

    Add files via upload

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Forgot from "./pages/Forgot";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
